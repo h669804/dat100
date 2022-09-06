c59034f106d5fc047d8d4980e4a336ecbb7efa75
Bruker <Bruker@158.37.227.144>
2022-09-06 20:37:59 +0200

    Endret feil med tegnsetting.

--- Innlevering1/src/no/hvl/dat100/B4.java
+++ Innlevering1/src/no/hvl/dat100/B4.java
@@ -26,16 +26,16 @@ public class B4 {
 		
 		if (bruttoLonn <= 164100 ) {
 			
-		} else if (bruttoLonn > 164101 && bruttoLonn < 23950) {
+		} else if (bruttoLonn >= 164101 && bruttoLonn <= 23950) {
 			sats = sats_1;
 			trinn = trinn_1;
-		} else if (bruttoLonn > 23951 && bruttoLonn < 580650) {
+		} else if (bruttoLonn >= 23951 && bruttoLonn <= 580650) {
 			sats = sats_2;
 			trinn = trinn_2;
-		} else if (bruttoLonn > 580651 && bruttoLonn < 934050) {
+		} else if (bruttoLonn >= 580651 && bruttoLonn <= 934050) {
 			sats = sats_3;
 			trinn = trinn_3;
-		} else if (bruttoLonn > 934051) {
+		} else if (bruttoLonn >= 934051) {
 			sats = sats_4;
 			trinn = trinn_4;
 		}
