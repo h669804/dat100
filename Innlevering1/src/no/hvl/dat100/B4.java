4d9d64e558909e1993dc6241e61c657edc9ea27e
Bruker <Bruker@158.37.227.144>
2022-09-07 11:57:17 +0200

    Forenklet koden ved å fjerne unødvendige variabler

--- Innlevering1/src/no/hvl/dat100/B4.java
+++ Innlevering1/src/no/hvl/dat100/B4.java
@@ -12,32 +12,23 @@ public class B4 {
 		int bruttoLonn = parseInt(belop);
 		
 		double sats = 0;
-		double sats_1 = 0.93;
-		double sats_2 = 2.41;
-		double sats_3 = 11.52;
-		double sats_4 = 14.52;
 		
 		int trinn = 0;
-		int trinn_1 = 1;
-		int trinn_2 = 2;
-		int trinn_3 = 3;
-		int trinn_4 = 4;
-		
 		
 		if (bruttoLonn <= 164100 ) {
 			
 		} else if (bruttoLonn >= 164101 && bruttoLonn <= 23950) {
-			sats = sats_1;
-			trinn = trinn_1;
+			sats = 0.93;
+			trinn = 1;
 		} else if (bruttoLonn >= 23951 && bruttoLonn <= 580650) {
-			sats = sats_2;
-			trinn = trinn_2;
+			sats = 2.41;
+			trinn = 2;
 		} else if (bruttoLonn >= 580651 && bruttoLonn <= 934050) {
-			sats = sats_3;
-			trinn = trinn_3;
+			sats = 11.52;
+			trinn = 3;
 		} else if (bruttoLonn >= 934051) {
-			sats = sats_4;
-			trinn = trinn_4;
+			sats = 14.52;
+			trinn = 4;
 		}
 		
 		System.out.println("Trinnskatt: " + trinn + " Sats: " + sats + "%");
