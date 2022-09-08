cfb36ae7f89eac3e7438844b0b6fa5831caec4d4
Bruker <Bruker@158.37.227.144>
2022-09-08 13:39:29 +0200

    Feilretting i system.out.print

--- Innlevering1/src/no/hvl/dat100/O3.java
+++ Innlevering1/src/no/hvl/dat100/O3.java
@@ -6,25 +6,33 @@ import static java.lang.Integer.*;
 public class O3 {
 
 	public static void main(String[] args) {
-		
+
 		final int N = parseInt(showInputDialog("n! når n er: "));
-		
+
 		int n = N;
 		int f = 1;
-		
-		if(n == 0) {
+
+		if (n == 0) {
 			f = 1;
 		}
-		while(n < 0) {
-		n = parseInt(showInputDialog("Ugyldig siffer. Prøv igjen: "));	
+		while (n < 0) {
+			n = parseInt(showInputDialog("Ugyldig siffer. Prøv igjen: "));
 		}
-		
-		System.out.print(N + "!");
-		
-		while(n >= 1) {
-			f = f*n;
+		if (N > 0) {
+			System.out.print(N + "!" + " = ");
+		} else {
+			System.out.print(N + "!");
+		}
+
+		while (n >= 1) {
+			f = f * n;
 			n--;
-			System.out.print(" = " + n+1 + "*");
+			if (n >= 1) {
+				System.out.print((n + 1) + "*");
+			} else {
+				System.out.print(n + 1);
+			}
+
 		}
 		System.out.println(" = " + f);
 	}
