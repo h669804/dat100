6f696fd724a2e8fefc5b5d7c74c32b27a3f5ba07
Bruker <Bruker@158.37.227.144>
2022-09-08 12:49:53 +0200

    Satt inn at n! = 1 * 2 * 3 * 4...

--- Innlevering1/src/no/hvl/dat100/O3.java
+++ Innlevering1/src/no/hvl/dat100/O3.java
@@ -7,9 +7,10 @@ public class O3 {
 
 	public static void main(String[] args) {
 		
-		int n = parseInt(showInputDialog("n! når n er: "));
-		int f = 1;
+		final int N = parseInt(showInputDialog("n! når n er: "));
 		
+		int n = N;
+		int f = 1;
 		
 		if(n == 0) {
 			f = 1;
@@ -17,10 +18,14 @@ public class O3 {
 		while(n < 0) {
 		n = parseInt(showInputDialog("Ugyldig siffer. Prøv igjen: "));	
 		}
-		while(n > 1) {
+		
+		System.out.print(N + "!");
+		
+		while(n >= 1) {
 			f = f*n;
 			n--;
+			System.out.print(" = " + n+1 + "*");
 		}
-		System.out.println("Fakultet av valgt siffer: " + f);
+		System.out.println(" = " + f);
 	}
 }
